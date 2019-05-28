b1927208faaedfb11bfb3d07cce794307872b3a8
waltergu <waltergu1989@gmail.com>
2019-05-28 14:41:50 +0800

    Add draft and thesis version.

--- paper/figure.py
+++ paper/figure.py
@@ -500,6 +500,220 @@ def v_spectrum():
     plt.savefig('v_spectrum.pdf')
     plt.close()
 
+def draft():
+    plt.ion()
+    fig,axes=plt.subplots(nrows=1,ncols=3)
+    fig.subplots_adjust(left=0.090,right=0.980,top=0.96,bottom=0.215,hspace=0.2,wspace=0.425)
+
+    ax=axes[0]
+    twinx=ax.twinx()
+    fbbp=np.loadtxt('../result/fbfm/S2x_BP_FBFM.dat')
+    twinx.plot(fbbp[:,0],np.abs(fbbp[:,1]),'-',color='green',lw=2,zorder=1)
+    twinx.plot(fbbp[:,0],np.abs(fbbp[:,2]),'--',color='green',lw=2,zorder=1)
+    twinx.minorticks_on()
+    twinx.set_ylim(-0.001,1.15)
+    twinx.set_yticks(np.linspace(0,1.0,3))
+    twinx.set_yticklabels(['0','$\\frac{\pi}{2}$','$\pi$'])
+    for tick in twinx.get_yticklabels():
+        tick.set_fontsize(18)
+        tick.set_color('green')
+    #twinx.set_ylabel('Berry Phase',color='green',fontdict={'fontsize':18})
+
+    fbgap=np.loadtxt('../result/fbfm/S2x_GAP_FBFM_EX.dat')
+    ax.plot(fbgap[:,0],fbgap[:,-1],color='blue',ls='-',lw=2,zorder=1)
+    ax.minorticks_on()
+    ax.set_xlim(0.0,0.8)
+    ax.set_ylim(0.0,0.1)
+    ax.set_xticks(np.linspace(0.0,0.8,5))
+    ax.set_xticklabels(['0.0','0.2','0.4','0.6','0.8'])
+    ax.set_yticks(np.linspace(0.0,0.1,6))
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(18)
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(18)
+        tick.set_color('blue')
+    ax.set_xlabel("$U_s/U_d$",fontdict={'fontsize':22})
+    ax.set_ylabel("$\Delta_\pi$",color='blue',fontdict={'fontsize':20})
+    ax.text(0.05,0.09,"(a)",fontsize=18,color='black',zorder=4)
+
+    ax=axes[1]
+    result=np.loadtxt('../result/fbfm/1DIF_S2x(60O-1O)_up_1.0_1.4_-0.04_1.0_FBFM_EDGE.dat')
+    xs=result[:,0]
+    ax.plot(xs,result[:,1:59]/xs[:,np.newaxis],color='grey',lw=1,alpha=0.5,zorder=1)
+    ax.plot(xs,result[:,59]/xs,color='blue',lw=2,zorder=2)
+    ax.plot(xs,result[:,60]/xs,color='green',lw=2,zorder=2)
+    ax.plot(xs,result[:,61]/xs,color='purple',lw=2,zorder=2)
+    ax.plot(xs,result[:,62]/xs,color='red',lw=2,zorder=2)
+    ax.plot(xs,result[:,63:110]/xs[:,np.newaxis],color='grey',lw=2,alpha=0.5,zorder=1)
+
+    ax.minorticks_on()
+    ax.set_ylim(0.19,0.6)
+    ax.set_xticks(np.linspace(0.0,0.8,5))
+    ax.set_yticks(np.linspace(0.2,0.6,5))
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(18)
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(18)
+    ax.set_xlabel("$U_s/U_d$",fontdict={'fontsize':20})
+    ax.set_ylabel("$E/Us$",fontdict={'fontsize':20})
+    ax.text(0.7,0.56,"(b)",fontsize=18,color='black')
+
+    ax=axes[2]
+    result=np.loadtxt('../result/fbfm/1DIF_S2x(60O-1O)_up_1.0_1.4_-0.04_0.78_1.0_FBFM_POS.dat')
+    ax.plot(result[:,0],result[:,2].real,color='blue',lw=2)
+    ax.plot(result[:,0],result[:,3].real,color='green',lw=2)
+    ax.plot(result[:,0],result[:,4].real,color='purple',lw=2)
+    ax.plot(result[:,0],result[:,5].real,color='red',lw=2)
+    ax.minorticks_on()
+    ax.set_xlim(0,120)
+    ax.set_ylim(0.0,0.45)
+    ax.set_xticks(np.linspace(0,120,4))
+    ax.set_yticks(np.linspace(0.0,0.4,3))
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(16)
+    ax.set_xlabel('position',fontdict={'fontsize':20})
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(18)
+    ax.set_ylabel('$\Delta\langle S_z\\rangle$',fontdict={'fontsize':18})
+    ax.text(10,0.4,"(c)",fontsize=18,color='black',va='center')
+
+    pdb.set_trace()
+    plt.savefig('draft.pdf')
+    plt.close()
+
+def delta_spectrum_thesis():
+    from mpl_toolkits.axes_grid.inset_locator import inset_axes
+    plt.ion()
+    gs=plt.GridSpec(2,4)
+    gs.update(left=0.075,right=0.925,top=0.975,bottom=0.145,hspace=0.2,wspace=0.18)
+
+    for i,(parameter,tag) in enumerate(zip([-0.04,0.18,0.26,0.41],['(a)','(b)','(c)','(d)'])):
+        data=np.load('../result/fbfm/1DIF_S2x(1P-1O)_up_1.0_1.4_%s_0.3_1.0_FBFM_EB800.npz'%HP.decimaltostr(parameter))['data']
+        ax=plt.subplot(gs[0,i%2]) if i<2 else plt.subplot(gs[1,i%2])
+        ax.plot(data[:,0],data[:,1:3],color='green',lw=2,zorder=2)
+        ax.plot(data[:,0],data[:,3:],color='grey',lw=2,alpha=0.3,zorder=1)
+
+        ax.minorticks_on()
+        ax.set_xlim(data[:,0].min(),data[:,0].max())
+        ax.set_ylim(0.0,0.25)
+        ax.set_xticks(np.linspace(0,800,5))
+        ax.set_xticklabels(['0','','$\pi$','','$2\pi$'])
+        if i>=2: ax.set_xlabel('q',fontdict={'fontsize':18})
+        ax.set_yticks(np.linspace(0.0,0.25,6))
+        ax.set_yticklabels(['0','','0.1','','0.2',''] if i in (0,2) else ['']*6)
+        if i in (0,2): ax.set_ylabel('$E/U_s$',fontdict={'fontsize':18})
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(18)
+        for tick in ax.get_yticklabels():
+            tick.set_fontsize(18)
+        ax.text(50,0.21,tag,fontsize=22,color='black')
+
+        ax=inset_axes(ax,width="40%",height=1.0,loc=1)
+        data=np.loadtxt('../result/tba/1DIF_S2x(1P-1O)_A_1.0_1.4_%s_TBA_EB.dat'%HP.decimaltostr(parameter))
+        ax.plot(data[:,0],data[:,1:],lw=1.5,color='blue',zorder=1)
+        ax.minorticks_on()
+        ax.set_ylim(-2.5,4.5)
+        ax.set_xticks([0,200,400])
+        ax.set_xticklabels(['$-\pi$','k','$\pi$'])
+        ax.set_yticks([-2.5,1.0,4.5])
+        ax.set_yticklabels(['-2.5','$E/t$','4.5'])
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(14)
+            tick.set_color('blue')
+        for tick in ax.get_yticklabels():
+            tick.set_fontsize(14)
+            tick.set_color('blue')
+
+    ax=plt.subplot(gs[:,2:4])
+    xs1=np.array([0.02,0.10,0.15,0.20,0.250,0.30,0.350,0.360,0.370,0.380,0.39,0.395,0.40,0.405,0.41])
+    ys1=np.array([0.36,0.33,0.31,0.28,0.255,0.22,0.175,0.165,0.155,0.145,0.13,0.120,0.11,0.100,0.00])
+    xs2=np.array([0.02,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.50,0.60,0.70,0.80])
+    ys2=np.array([0.38,0.41,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.50,0.51])
+    ax.plot(xs1,ys1,lw=2)
+    ax.plot(xs2,ys2,lw=2)
+    ax.scatter([0.3,0.3,0.3,0.3],[0.0,0.22,0.30,0.45],s=100,color='red',marker='*',edgecolors='none',zorder=4,clip_on=False,alpha=0.8)
+    ax.minorticks_on()
+    ax.set_xlim(0.1,0.7)
+    ax.set_ylim(0.0,0.6)
+    ax.set_xticks(np.linspace(0.1,0.7,4))
+    ax.set_xticklabels(['0.1','0.3','0.5','0.7'])
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    ax.set_yticks(np.linspace(0.0,0.6,4))
+    ax.set_yticklabels(['0','0.2','0.4','0.6'])
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(18)
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(18)
+    ax.set_xlabel("$U_s/U_d$",fontdict={'fontsize':20})
+    ax.set_ylabel("$\Delta/t$",fontdict={'fontsize':20})
+    ax.text(0.20,0.10,"FM",fontsize=22,color='black')
+    ax.text(0.45,0.20,"TFM",fontsize=22,color='black')
+    ax.text(0.15,0.48,"NFM",fontsize=22,color='black')
+    ax.text(0.60,0.52,"(e)",fontsize=22,color='black',zorder=4)
+
+    pdb.set_trace()
+    plt.savefig('delta_spectrum_thesis.pdf')
+    plt.close()
+
+def v_spectrum_thesis():
+    plt.ion()
+    gs=plt.GridSpec(2,4)
+    gs.update(left=0.075,right=0.925,top=0.975,bottom=0.145,hspace=0.2,wspace=0.18)
+
+    for i,(parameter,tag) in enumerate(zip([0.0,0.09,0.4,0.64],['(a)','(b)','(c)','(d)'])):
+        data=np.load('../result/fbfm/1DIF_1.0_1.25_-0.4375_0.3_1.0_%s_EB800.npz'%HP.decimaltostr(parameter))['data']
+        ax=plt.subplot(gs[0,i%2]) if i<2 else plt.subplot(gs[1,i%2])
+        ax.plot(data[:,0],data[:,1:3],color='green',lw=2,zorder=2)
+        ax.plot(data[:,0],data[:,3:],color='grey',lw=2,alpha=0.3,zorder=1)
+
+        ax.minorticks_on()
+        ax.set_ylim(0.0,0.30)
+        ax.set_xlim(data[:,0].min(),data[:,0].max())
+        ax.set_xticks(np.linspace(0,800,5))
+        ax.set_xticklabels(['0','','$\pi$','','$2\pi$'])
+        ax.set_xlabel('q',fontdict={'fontsize':18})
+        ax.set_yticks(np.linspace(0.0,0.3,4))
+        ax.set_yticklabels(['0','0.1','0.2','0.3'] if i in (0,2) else ['']*6)
+        if i in (0,2): ax.set_ylabel('$E/U_s$',fontdict={'fontsize':18})
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(18)
+        for tick in ax.get_yticklabels():
+            tick.set_fontsize(18)
+        ax.text(50,0.25,tag,fontsize=22,color='black')
+
+    ax=plt.subplot(gs[:,2:4])
+    xs1=np.array([0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.71])
+    ys1=np.array([0.05,0.08,0.09,0.09,0.08,0.05,0.01,0.00])
+    xs2=np.array([0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90])
+    ys2=np.array([0.38,0.54,0.64,0.72,0.81,0.85,0.89,0.93,0.97])
+    ax.plot(xs1,ys1,lw=2)
+    ax.plot(xs2,ys2,lw=2)
+    ax.scatter([0.3,0.3,0.3,0.3],[0.00,0.09,0.40,0.64],s=100,color='red',marker='*',edgecolors='none',zorder=4,clip_on=False,alpha=0.8)
+    ax.minorticks_on()
+    ax.set_xlim(0.1,0.9)
+    ax.set_ylim(0.0,1.0)
+    ax.set_xticks(np.linspace(0.1,0.9,5))
+    ax.set_xticklabels(['0.1','0.3','0.5','0.7','0.9'])
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    ax.set_yticks(np.linspace(0.0,1.0,6))
+    ax.set_yticklabels(['0','0.2','0.4','0.6','0.8','1.0'])
+    for tick in ax.get_xticklabels():
+        tick.set_fontsize(18)
+    for tick in ax.get_yticklabels():
+        tick.set_fontsize(18)
+    ax.set_xlabel("$U_s/U_d$",fontdict={'fontsize':20})
+    ax.set_ylabel("$V/U_d$",fontdict={'fontsize':20})
+    ax.text(0.35,0.01,"FM",fontsize=22,color='black')
+    ax.text(0.50,0.35,"TFM",fontsize=22,color='black')
+    ax.text(0.15,0.75,"NFM",fontsize=22,color='black')
+    ax.text(0.80,0.83,"(e)",fontsize=22,color='black',zorder=4)
+
+    pdb.set_trace()
+    plt.savefig('v_spectrum_thesis.pdf')
+    plt.close()
+
 if __name__=='__main__':
     for arg in sys.argv:
         if arg in ('1','all'): lattice()
@@ -509,3 +723,6 @@ if __name__=='__main__':
         if arg in ('5','all'): delta_spectrum()
         if arg in ('6','all'): v_spectrum()
         if arg in ('7','all'): lattice_poster()
+        if arg in ('8','all'): draft()
+        if arg in ('9','all'): delta_spectrum_thesis()
+        if arg in ('0','all'): v_spectrum_thesis()
